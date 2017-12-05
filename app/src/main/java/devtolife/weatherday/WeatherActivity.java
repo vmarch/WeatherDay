aae4eb03bd1427dda1209ec413b946ddae04c884
Vovas <marchenkoukr@gmail.com>
2017-12-06 00:03:41 +0200

    New update.

--- app/src/main/java/devtolife/weatherday/WeatherActivity.java
+++ app/src/main/java/devtolife/weatherday/WeatherActivity.java
@@ -1,30 +1,42 @@
 package devtolife.weatherday;
 
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import devtolife.weatherday.fragments.DataFragment;
+import devtolife.weatherday.privacy_policy.PrivacyPolicy;
 
 
 public class WeatherActivity extends AppCompatActivity {
+    EditText editCity;
+    Button btnCheckWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
-        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .add(R.id.data_fragment, new DataFragment())
-//                    .commit();
-        }
+        editCity = findViewById(R.id.edit_city);
+        editCity.setInputType(InputType.TYPE_CLASS_TEXT);
 
+        btnCheckWeather = findViewById(R.id.btn_check_weather);
+        btnCheckWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newCity = editCity.getText().toString();
+                if (!newCity.equals("")) {
+                    changeCity(newCity);
+                    editCity.setText("");
+                }
+            }
+        });
     }
 
     @Override
@@ -36,33 +48,22 @@ public class WeatherActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.change_city) {
-            showInputDialog();
-        }
-        return false;
-    }
+        Intent intent;
 
-    private void showInputDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Change city");
-        final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        builder.setView(input);
-        builder.setPositiveButton("Go", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                changeCity(input.getText().toString());
-            }
-        });
-        builder.show();
+        if (item.getItemId() == R.id.action_policy) {
+            intent = new Intent(this, PrivacyPolicy.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return false;
+
     }
 
     public void changeCity(String city) {
         DataFragment wf = (DataFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.data_fragment);
-        wf.changeCity(city);
+        wf.changeCityFragm(city);
         new CityPreference(this).setCity(city);
     }
-
-
 }
--- app/src/main/java/devtolife/weatherday/fragments/DataFragment.java
+++ app/src/main/java/devtolife/weatherday/fragments/DataFragment.java
@@ -32,7 +32,6 @@ public class DataFragment extends Fragment {
     TextView humidityField;
     public Locale myLocale;
     private int dayTime;
-    private int curentTime;
     private ImageView weatherIcon;
     private int icon;
     Handler handler;
@@ -50,14 +49,14 @@ public class DataFragment extends Fragment {
         myLocale = new Locale("bel", "BY");
 
         View rootView = inflater.inflate(R.layout.data_fragment, container, false);
-        weatherIcon = (ImageView) rootView.findViewById(R.id.image_weather);
-        dateField = (TextView) rootView.findViewById(R.id.text_date_value);
-        cityField = (TextView) rootView.findViewById(R.id.text_city_value);
-        temperField = (TextView) rootView.findViewById(R.id.text_temper_value);
-        cloudsField = (TextView) rootView.findViewById(R.id.text_clouds_value);
-        humidityField = (TextView) rootView.findViewById(R.id.text_humidity_value);
-        pressureField = (TextView) rootView.findViewById(R.id.text_pressure_value);
-        windField = (TextView) rootView.findViewById(R.id.text_wind_value);
+        weatherIcon = rootView.findViewById(R.id.image_weather);
+        dateField = rootView.findViewById(R.id.text_date_value);
+        cityField = rootView.findViewById(R.id.text_city_value);
+        temperField = rootView.findViewById(R.id.text_temper_value);
+        cloudsField = rootView.findViewById(R.id.text_clouds_value);
+        humidityField = rootView.findViewById(R.id.text_humidity_value);
+        pressureField = rootView.findViewById(R.id.text_pressure_value);
+        windField = rootView.findViewById(R.id.text_wind_value);
 
         return rootView;
     }
@@ -142,7 +141,7 @@ public class DataFragment extends Fragment {
 
     private void setWeatherIcon(int actualId, int daySun) {
         int id = actualId / 100;
-               if (id == 8) {
+        if (id == 8) {
             switch (actualId) {
                 case 800:
                     if (daySun == 0) {
@@ -159,12 +158,6 @@ public class DataFragment extends Fragment {
                     }
                     break;
                 case 802:
-                    if (daySun == 0) {
-                        icon = R.drawable.clouds0;
-                    } else {
-                        icon = R.drawable.clouds1;
-                    }
-                    break;
                 case 803:
                     if (daySun == 0) {
                         icon = R.drawable.clouds0;
@@ -184,17 +177,6 @@ public class DataFragment extends Fragment {
                 icon = R.drawable.drizzle1;
             }
 
-//            300	light intensity drizzle	легка мряка
-//            301	drizzle	мряка
-//            302	heavy intensity drizzle	сильна мряка
-//            310	light intensity drizzle rain	легкий дощ з мрякою
-//            311	drizzle rain	дощ і мряка
-//            312	heavy intensity drizzle rain	сильний дощ і мряка
-//            313	shower rain and drizzle	проливний дощ і мряка
-//            314	heavy shower rain and drizzle	сильний проливний дощ з мрякою
-//            321	shower drizzle	проливна мряка
-
-
         } else if (id == 5) {
             switch (actualId) {
                 case 500:
@@ -205,12 +187,6 @@ public class DataFragment extends Fragment {
                     }
                     break;
                 case 501:
-                    if (daySun == 0) {
-                        icon = R.drawable.showers0;
-                    } else {
-                        icon = R.drawable.showers1;
-                    }
-                    break;
                 case 502:
                     if (daySun == 0) {
                         icon = R.drawable.showers0;
@@ -218,56 +194,27 @@ public class DataFragment extends Fragment {
                         icon = R.drawable.showers1;
                     }
                     break;
-                case 503:
-                    icon = R.drawable.showers;
-                    break;
-                case 504:
-                    icon = R.drawable.showers;
-                    break;
+
                 case 511:
                     icon = R.drawable.freezing_rain;
                     break;
-                case 520:
-                    icon = R.drawable.showers_scattered;
-                    break;
+                case 503:
+                case 504:
                 case 521:
-                    icon = R.drawable.showers;
-                    break;
                 case 522:
                     icon = R.drawable.showers;
                     break;
+                case 520:
                 case 531:
                     icon = R.drawable.showers_scattered;
                     break;
 
-//            500	light rain	невеликий дощ
-//            501	moderate rain	помірний дощ
-//            502	heavy intensity rain	сильний дощ
-//            503	very heavy rain	дуже сильний дощ
-//            504	extreme rain	надзвичайно сильний дощ
-//            511	freezing rain	дощ з обледенінням
-//            520	light intensity shower rain	легкий проливний дощ
-//            521	shower rain	проливний дощ
-//            522	heavy intensity shower rain	сильний проливний дощ
-//            531	ragged shower rain	поривчастий проливний дощ
             }
 
         } else if (id == 6) {
             switch (actualId) {
                 case 600:
-                    if (daySun == 0) {
-                        icon = R.drawable.snow_scattered0;
-                    } else {
-                        icon = R.drawable.snow_scattered1;
-                    }
-                    break;
                 case 601:
-                    if (daySun == 0) {
-                        icon = R.drawable.snow_scattered0;
-                    } else {
-                        icon = R.drawable.snow_scattered1;
-                    }
-                    break;
                 case 602:
                     if (daySun == 0) {
                         icon = R.drawable.snow_scattered0;
@@ -276,36 +223,16 @@ public class DataFragment extends Fragment {
                     }
                     break;
                 case 611:
-                    icon = R.drawable.snow_rain;
-                    break;
                 case 612:
-                    icon = R.drawable.snow_rain;
-                    break;
                 case 615:
-                    icon = R.drawable.snow_rain;
-                    break;
                 case 616:
                     icon = R.drawable.snow_rain;
                     break;
                 case 620:
-                    icon = R.drawable.snow;
-                    break;
                 case 621:
-                    icon = R.drawable.snow;
-                    break;
                 case 622:
                     icon = R.drawable.snow;
                     break;
-//                600	light snow	легкий сніг
-//                601	snow	сніг
-//                602	heavy snow	сильний сніг
-//                611	sleet	дощ зі снігом, ожеледиця
-//                612	shower sleet	проливний дощ зі снігом, ожеледиця
-//                615	light rain and snow	невеликий дощ зі снігом
-//                616	rain and snow	дощ зі снігом
-//                620	light shower snow	легкий рясний сніг
-//                621	shower snow	рясний сніг
-//                622	heavy shower snow	сильний рясний сніг
 
             }
         } else if (id == 7) {
@@ -324,27 +251,10 @@ public class DataFragment extends Fragment {
                 icon = R.drawable.tornado;
             }
 
-//            701	mist	туман
-//            711	smoke	дим
-//            721	haze	імла
-//            731	sand, dust whirls	пісок, пилові вихори
-//            741	fog	туман
-//            751	sand	пісок
-//            761	dust	пил
-//            762	volcanic ash	вулканічний попіл
-//            771	squalls	шквал
-//            781	tornado	торнадо
-
         } else if (id == 9) {
             switch (actualId) {
                 case 900:
                     icon = R.drawable.tornado;
-                    break;
-                case 901:
-                    icon = R.drawable.storm;
-                    break;
-                case 902:
-                    icon = R.drawable.storm;
                     break;
                 case 903:
                     icon = R.drawable.cold;
@@ -352,44 +262,14 @@ public class DataFragment extends Fragment {
                 case 904:
                     icon = R.drawable.clear_sky0;
                     break;
-                case 905:
-                    if (daySun == 0) {
-                        icon = R.drawable.clear_sky0;
-                    } else {
-                        icon = R.drawable.clear_sky1;
-                    }
-                    break;
                 case 906:
                     icon = R.drawable.hail;
                     break;
+                case 905:
                 case 951:
-                    if (daySun == 0) {
-                        icon = R.drawable.clear_sky0;
-                    } else {
-                        icon = R.drawable.clear_sky1;
-                    }
-                    break;
                 case 952:
-                    if (daySun == 0) {
-                        icon = R.drawable.clear_sky0;
-                    } else {
-                        icon = R.drawable.clear_sky1;
-                    }
-                    break;
                 case 953:
-                    if (daySun == 0) {
-                        icon = R.drawable.clear_sky0;
-                    } else {
-                        icon = R.drawable.clear_sky1;
-                    }
-                    break;
                 case 954:
-                    if (daySun == 0) {
-                        icon = R.drawable.clear_sky0;
-                    } else {
-                        icon = R.drawable.clear_sky1;
-                    }
-                    break;
                 case 955:
                     if (daySun == 0) {
                         icon = R.drawable.clear_sky0;
@@ -398,19 +278,7 @@ public class DataFragment extends Fragment {
                     }
                     break;
                 case 956:
-                    if (daySun == 0) {
-                        icon = R.drawable.storm0;
-                    } else {
-                        icon = R.drawable.storm1;
-                    }
-                    break;
                 case 957:
-                    if (daySun == 0) {
-                        icon = R.drawable.storm0;
-                    } else {
-                        icon = R.drawable.storm1;
-                    }
-                    break;
                 case 958:
                     if (daySun == 0) {
                         icon = R.drawable.storm0;
@@ -418,42 +286,19 @@ public class DataFragment extends Fragment {
                         icon = R.drawable.storm1;
                     }
                     break;
+                case 901:
+                case 902:
                 case 959:
-                    icon = R.drawable.storm;
-                    break;
                 case 960:
-                    icon = R.drawable.storm;
-                    break;
                 case 961:
-                    icon = R.drawable.storm;
-                    break;
                 case 962:
                     icon = R.drawable.storm;
                     break;
-//            900	tornado	торнадо
-//            901	tropical storm	тропічний шторм
-//            902	hurricane	ураган
-//            903	cold	холодно
-//            904	hot	гаряче
-//            905	windy	вітряно
-//            906	hail	град
-//            951	calm	штиль
-//            952	light breeze	легкий бриз
-//            953	gentle breeze	ніжний бриз
-//            954	moderate breeze	помірний бриз
-//            955	fresh breeze	свіжий бриз
-//            956	strong breeze	сильний бриз
-//            957	high wind, near gale	сильний вітер, майже шторм
-//            958	gale	шторм
-//            959	severe gale	сильний шторм
-//            960	storm	шторм
-//            961	violent storm	сильна буря
-//            962	hurricane	ураган
             }
         }
-            }
+    }
 
-    public void changeCity(String city) {
+    public void changeCityFragm(String city) {
         updateWeatherData(city);
     }
 
